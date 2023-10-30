5e0e51aef155c938b6da8ce622570121c5d2f49f
JackWalton1 <jwalto02@calpoly.edu>
2023-10-29 19:23:00 -0700

    added code

---
+++ demoCode.py
@@ -0,0 +1,5 @@
+import math
+# Python 3 program to find factorial of a number
+def factorial(n):
+	return(math.factorial(n))
+
---
+++ demoTests.py
@@ -0,0 +1,18 @@
+import unittest
+import demoCode
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_zero(self):
+        self.assertEqual(demoCode.factorial(0), 1)
+
+    def test_postives(self):
+        self.assertEqual(demoCode.factorial(1), 1)
+        self.assertEqual(demoCode.factorial(2), 2)
+        self.assertEqual(demoCode.factorial(5), 120)
+        self.assertEqual(demoCode.factorial(6), 720)
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
