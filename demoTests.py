431048d8b0f8303e0f799424789c33b457c7bb79
JackWalton1 <jwalto02@calpoly.edu>
2023-10-31 12:37:39 -0700

    stuff

--- demoTests.py
+++ demoTests.py
@@ -12,5 +12,6 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(demoCode.factorial(5), 120)
         self.assertEqual(demoCode.factorial(6), 720)
 
+
 if __name__ == '__main__':
     unittest.main()
